2a28fd1db38b55ac550327f578aa7fcec0dd8d1b
Alexandre <veladorisz@gmail.com>
2023-01-12 08:03:33 -0300

    adicionado Css e preparado para futuras alerações na logica

--- func.php
+++ func.php
@@ -3,19 +3,20 @@ function buscaNoticia($url,$hq){
     
 libxml_use_internal_errors(true);
 
-$conteudo = file_get_contents(filename: 'https://www.'.$url.'.com/');
+$conteudo = file_get_contents(filename: 'https://'.$url.'.com/');
 $document = new DOMDocument;
 $document->loadHTML($conteudo);
 
 $xPath = new DOMXPath($document);
 $domNodeList = $xPath->query(expression: './/div[@class="'.$hq.'"]');
-
-echo "<table>
+echo "<table class='coisa'>
  <tr>Resultados:</tr>";
 foreach($domNodeList as $elemento){
         $virus= $elemento->textContent;
         $xxt =$elemento->firstElementChild->getAttribute('href');
-
+        if(!$xxt){
+            $xxt=$elemento->firstElementChild->firstElementChild->getAttribute('href');
+        }
         echo "<tr></tr><td><a href='$xxt'>$virus</a></td></tr>";
          
 }
--- index.php
+++ index.php
@@ -4,20 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    include './func.php';
-    if(isset($_GET['acao'])){
-        $result = 'post ';
-        $nome = $_GET['nome'];
-        buscaNoticia($nome, $result);
-    }
-    ?>
-    <form style="display: flex; align-items: center; justify-content: center; margin:auto; height: 900px; ">
-        <input type="text" name="nome" placeholder="Jorge é gay...">
+    
+    <form class="inpus">
+        <input type="text" name="nome" placeholder="escolha a emissora...">
         <input type="submit" name="acao" value="Pesquisar">
     </form>
+    <div class="texto">
+
+    <?php
+    include './func.php';
+    if (isset($_GET['acao'])) {
+        $nome = $_GET['nome'];
+        if ($nome == 'globo') {
+            $result = 'post ';
+            buscaNoticia($nome, $result);
+        }if ($nome == 'g1.globo'){
+            $result = '_evt';
+            buscaNoticia($nome, $result);
+        }else {
+            echo "note";
+        }
+    }
+    ?>
+    </div>
 </body>
 </html>
